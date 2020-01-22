04bcc1c1adf16ab3681a9eb9c37d06f1122d2971
Rodrigo <rodrigo_bervig@hotmail.com>
2020-01-22 12:07:11 -0300

    Solution lesson 2 #1

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,19 @@
 import { Router } from 'express'
 
+const controllers = (req, res) => {
+  res.send({ message: 'hello'})
+}
+
 const router = Router()
+
+router
+  .route('/id')
+  .get(controllers)
+  .post(controllers)
+
+router
+  .route('/:id')
+  .put(controllers)
+  .delete(controllers)
 
 export default router
