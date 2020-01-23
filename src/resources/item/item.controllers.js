abe1df51bf7ef67bd191742ab5b615b5b49af79e
Rodrigo <rodrigo_bervig@hotmail.com>
2020-01-23 17:17:27 -0300

    Solution to lesson 3 #1

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,33 @@
-export default {}
+import { Item } from './item.model'
+import mongoose from 'mongoose'
+import { connect } from '../../utils/db'
+
+const run = async () => {
+  await connect('mongodb://localhost:27017/api-test')
+  const item = await Item.create({
+    name: 'Clean up',
+    createdBy: mongoose.Types.ObjectId(),
+    list: mongoose.Types.ObjectId()
+  })
+
+  const updated = await Item.findByIdAndUpdate(
+    item._id,
+    { name: 'eat' },
+    { new: true }
+  ).exec()
+
+  console.log(updated)
+}
+
+connect('mongodb://localhost:21017/api-test')
+  .then(
+    Item.find({})
+      .exec()
+      .then(response => {
+        console.log('hey! \n' + response)
+      })
+      .catch(err => console.log('fuck' + err))
+  )
+  .catch(err => console.log('oops: \n' + err))
+
+// run()
